ded73b6bdc719949cd2ace6e436c2055451695f8
Robinson <danny.2.robinson@bt.com>
2017-02-21 12:42:53 +0000

    DR - Ticket generation

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TicketBook from './TicketBook';
-import bingoTicket from '../fakeDB/bingoTicket';
+import bingoTicketApi from '../fakeDB/bingoTicket';
 
 const App = ({ text }) => (
-  <div>
-		TicketBook: {text}
-    <TicketBook book={bingoTicket} />
-  </div>
+  <span>
+      <TicketBook book={bingoTicketApi.provideBook()} />
+  </span>
 );
+
+/*generate will be moved to purchase component*/
+/*Book will then be grabbed from mongodb*/
 
 export default App;
