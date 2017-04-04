243e88aa5d9bb0d0371b309cf60cafaa7d5fd1b3
Robinson <danny.2.robinson@bt.com>
2017-04-04 15:02:32 +0100

    Moved code to server.js, fixed server crashes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const mongoApi = require("./src/fakeDB/mongoApi");
 const bingoTicket = require("./src/fakeDB/bingoTicket");
+const ldap = require('ldapjs');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 module.exports = (app, port) => {
   app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -152,4 +155,53 @@ module.exports = (app, port) => {
             getGuestName: getGuestName
         };
     }());
+    app.use(bodyParser.urlencoded ({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+
+    var adminClient = ldap.createClient({
+        url: 'ldap://iuserldap.nat.bt.com'
+    });
+
+    var serverLdap = ldap.createServer();
+
+    serverLdap.listen(389, function() {
+        console.log('server is up');
+    });
+
+    app.post('/activeTickets', function(req, res) {
+        let username = req.body.username;
+        let password = req.body.password;
+        let btDN = 'CN=' + username + ',OU=employee,OU=btplc,DC=iuser,DC=iroot,DC=adidom,DC=com';
+
+        adminClient.bind(btDN, password, function(err) {
+
+            if (err !== null) {
+
+                if (err.name === "InvalidCredentialsError") {
+                    res.sendStatus(401);
+                } else {
+                    res.send("Unknown error: " + JSON.stringify(err));
+                }
+            } else {
+                res.send(JSON.stringify(generateSessionID(username, password)));
+            }
+        });
+
+        function generateSessionID(username, password) {
+            var milliseconds = Date.now();
+            sessionID = username + milliseconds + password;
+            return crypto.createHmac('sha256', generateKey()).update(sessionID).digest('hex');
+        }
+
+        function generateKey() {
+            return crypto.randomBytes(32).toString('base64');
+        }
+
+    });
+
+
 };
